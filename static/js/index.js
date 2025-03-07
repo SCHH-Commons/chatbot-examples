2baac54dec3e6db05df2699a9cacb800b57cc6d1
Ron Snyder <ron@snyderjr.com>
2025-03-07 11:00:56 -0500

    testing

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,13 @@
 console.log('index.js')
 console.log(window.contentSelector)
 
-console.log(document.querySelector(window.contentSelector))
+let contentEl = document.querySelector(window.contentSelector)
+if (contentEl) {
+  Array.from(contentEl.querySelectorAll('p > code')).forEach((codeEl) => {
+    console.log(codeEl)
+    if (codeEl.trim().startsWith('include')) {
+      let src = codeEl.textContent.split(' ').filter(pe => pe)[1]
+      codeEl.innerHTML = src
+    }
+  })
+}
